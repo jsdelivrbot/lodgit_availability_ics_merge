41ccadb8eb69515f2ec471d5c3631307b787eca1
Jacob <jacob@comcuro.com>
2017-07-16 23:25:56 +0200

    moment.js reference push problem solved

--- main.js
+++ main.js
@@ -73,13 +73,16 @@ var rooms = {
 
 var pools = {
     luxus: {
-        merged: []
+        merged_moment: [],
+        merged_str: []
     },
     comfort: {
-        merged: []
+        merged_moment: [],
+        merged_str: []
     },
     luxus2sz: {
-        merged: []
+        merged_moment: [],
+        merged_str: []
     }
 }
 
@@ -94,8 +97,8 @@ var reg = /BEGIN:VEVENT\nDTSTART:([0-9]{8})\nDTEND:([0-9]{8})/g;
 var fn = function (key, i, arr){
     return new Promise((resolve,reject) => {
         request(rooms[key].ics_lnk, function (error, response, body) {
-            if (error) {
-                resolve(false);
+            //console.log(key + ": statuscode=" + response.statusCode);
+            if (error || response.statusCode != 200) {
                 reject(true);
                 return false;
             }
@@ -113,7 +116,7 @@ var fn = function (key, i, arr){
 
                 while (res){
                     res = reg.exec(rooms[key].ics_txt);
-                    if (res==null){break;}
+                    if (res==null) {break;}
 
                     //console.log(res);
                     rooms[key].evts.push({begin: moment(res[1], "YYYYMMDD"), end: moment(res[2], "YYYYMMDD")});
@@ -125,13 +128,17 @@ var fn = function (key, i, arr){
     });
 }
 
-var promises = Object.keys(rooms).map(fn);
+var promises = Object.keys(rooms).filter((r_key) => rooms[r_key].enabled).map(fn);
 
 var p_result = Promise.all(promises);
 
 p_result.then(function(){
     //log_rooms();
     mergeAllPools();
+});
+
+p_result.catch(function(){
+    console.log("not every promise fulfilled - not all roomes catched");
 });
 
 function log_rooms(){
@@ -151,8 +158,31 @@ function log_rooms(){
 
 function mergeAllPools(){
     //merge("comfort");
+    console.log("_______ luxus _______________________________________________________________________________");
     merge("luxus");
+    // printPool("luxus");
+    console.log("_______ comfort ______________________________________________________________________________");
+    merge("comfort");
+    // printPool("comfort");
+    console.log("_______ luxus2sz _____________________________________________________________________________");
+    merge("luxus2sz");
+    // printPool("luxus2sz");
     //merge("luxus2z");
+    mergeIntoPeriods("luxus");
+    mergeIntoPeriods("comfort");
+    mergeIntoPeriods("luxus2sz");
+}
+
+function printPool(poolName){
+    //console.log(pools[poolName].merged);
+    console.log("merged_moment --------- ");
+    pools[poolName].merged_moment.map(function(o,i){
+        console.log("[" + i + "] " + o.format("YYYY.MM.DD"));
+    });
+    console.log("merged_str --------- ");
+    pools[poolName].merged_str.map(function(o,i){
+        console.log("[" + i + "] " + o);
+    });
 }
 
 function merge(poolName){
@@ -160,48 +190,73 @@ function merge(poolName){
     Object.keys(rooms).forEach((r_key,i,arr)=>{
         if (!rooms[r_key].enabled) return;
         if (rooms[r_key].pool!=poolName) return;
-        if (firstDone)return;
+        if (firstDone)return;   //only do first in the pool and then compare all reserved day against the reserved periods in the other rooms 
         firstDone=true;
         console.log("\n---- " + r_key + " --------------------------------------------------");
-
+        //iterate through all reserved time periods
         rooms[r_key].evts.forEach((e,i,arr)=>{
             console.log(e.begin.format("YYYY.MM.DD") + " - " + e.end.format("YYYY.MM.DD"));
-            if (i == 0) return;
-            if (i == arr.length -1) return;
+            if (i == 0) return;               //first one is always from 1970 to now
+            if (i == arr.length -1) return;   //last one is always from now to in two years
             var iD = e.begin;
-            while(iD.isSameOrBefore(e.end)){
-                console.log(iD.format("YYYY.MM.DD"));
+            while(iD.isSameOrBefore(e.end)){    //iterate through all days in the given period
                 //does this day occur in any other reserved timperiod of other rooms in this pool
+                if (!oneOfAllOtherRoomsAvailable(poolName, r_key, iD)){
+                    console.log("   " + iD.format("YYYY.MM.DD") + " - overlap");
+                    pools[poolName].merged_moment.push(moment(iD));
+                    pools[poolName].merged_str.push(iD.format("YYYY.MM.DD"));
+                } else {
+                    console.log("   " + iD.format("YYYY.MM.DD") + "");
+                }
                 iD.add(1,"d");
             }
         });
     });
 }
 
-function reservedInEveryOtherRoom(poolName){
-    var firstDone = false;
+function oneOfAllOtherRoomsAvailable(poolName,exclude_key,day){
+    var oneRoomVacant = false;
+    //iterate through all rooms
     Object.keys(rooms).forEach((r_key,i,arr)=>{
         if (!rooms[r_key].enabled) return;
-        if (rooms[r_key].pool!=poolName) return;
-        if (firstDone)return;
-        firstDone=true;
-        console.log("\n---- " + r_key + " --------------------------------------------------");
-
+        if (rooms[r_key].pool!=poolName) return;    //only process rooms in specified pool
+        if (r_key == exclude_key)return;    //do not check the comparison key
+        //console.log("\n---- " + r_key + " --------------------------------------------------");
+        //iterate through all reserved time periods
+        var overlap = false;
         rooms[r_key].evts.forEach((e,i,arr)=>{
-            console.log(e.begin.format("YYYY.MM.DD") + " - " + e.end.format("YYYY.MM.DD"));
-            if (i == 0) return;
-            if (i == arr.length -1) return;
-            var iD = e.begin;
-            while(iD.isSameOrBefore(e.end)){
+            //console.log(e.begin.format("YYYY.MM.DD") + " - " + e.end.format("YYYY.MM.DD"));
+            if (i == 0) return;               //first one is always from 1970 to now
+            if (i == arr.length -1) return;   //last one is always from now to in two years
+            let begin = day.isSameOrAfter(e.begin);
+            let end = day.isSameOrBefore(e.end);
+            if (day.isSameOrAfter(e.begin) && day.isSameOrBefore(e.end)){
+                overlap = true;
+                return true;
+            }
+            /*while(iD.isSameOrBefore(e.end)){
                 console.log(iD.format("YYYY.MM.DD"));
                 //does this day occur in any other reserved timperiod of other rooms in this pool
                 iD.add(1,"d");
-            }
+            }*/
         });
+        if (!overlap) {oneRoomVacant=true}
     });
+    return oneRoomVacant;
 }
 
+function mergeIntoPeriods(poolName){
+    // var dayBefore = {};
+    var dayBefore = moment(pools[poolName].merged_moment[0]).subtract(7,"days");
 
+    pools[poolName].merged_moment.map(function(m,i){
+        console.log("diff[" + i + "]: " + m.format("YYYY.MM.DD") + ": " + dayBefore.diff(m,"days"));
+        if(dayBefore.diff(m,"days") == -1) {
+            //console.log("diff = -1");
+        }
+        dayBefore = moment(m);
+    });
+}
 
 //compare AND and merge in obj
 
